d6a4e8820d1caae259378d5a4af1251c13d1aea6
Jon Austin <jon.i.austin@gmail.com>
2023-05-18 12:30:55 -0700

    Terragrunt needs the provider defined in the module (doublecheck this though)

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+# Terragrunt needs the provider defined in the module (doublecheck this though)
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = "${var.prefix}-${var.name}"
 
